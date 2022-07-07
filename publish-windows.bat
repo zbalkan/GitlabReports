0c31521c5a3b658de639aa1ca58d1d6639c45280
Zafer Balkan <zafer@zaferbalkan.com>
2022-07-07 14:39:13 +0300

    Fixed output in publish script

--- publish-windows.bat
+++ publish-windows.bat
@@ -1,1 +1,1 @@
-dotnet publish src/SemgrepReports.csproj --framework net6.0 --runtime win-x64 -c Release -o "SemgrepReports-v1.0-$windows-x64-self-contained" --self-contained true -p:PublishTrimmed=true -p:PublishSingleFile=true
+dotnet publish src/SemgrepReports.csproj --framework net6.0 --runtime win-x64 -c Release -o "SemgrepReports-windows-x64-self-contained" --self-contained true -p:PublishTrimmed=true -p:PublishSingleFile=true
