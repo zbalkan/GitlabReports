cce8eb3cc5b77c9a6225f2277127648e09ba9090
Zafer Balkan <zafer@zaferbalkan.com>
2022-08-09 20:20:13 +0300

    Used Spectre.console and removed Utility.Commandline (#8)
    * Used Spectre.console and removed Utility.Commandline
    
    * Changed application name shown on help output

---
+++ src/GenerateReportCommand.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using Spectre.Console;
+using System.IO;
+using Spectre.Console.Cli;
+using System;
+using System.Threading;
+
+namespace GitlabReports
+{
+    public sealed class GenerateReportCommand : Command<GenerateReportCommand.Settings>
+    {
+        public override int Execute([NotNull] CommandContext context, [NotNull] Settings settings)
+        {
+            var output = settings.OutputFile;
+            if (string.IsNullOrEmpty(settings.OutputFile))
+            {
+                output = Path.ChangeExtension(settings.InputFile, ".pdf");
+            }
+
+            AnsiConsole.Status()
+                .AutoRefresh(true)
+                .Spinner(Spinner.Known.BouncingBar)
+                .SpinnerStyle(Style.Parse("green bold"))
+                .Start("Processing...",
+                    ctx =>
+                    {
+                        try
+                        {
+                            ctx.Status("Importing JSON file...");
+                            var report = ReportGenerator.Import(settings.InputFile);
+                            Thread.Sleep(1000);
+                            ctx.Status("Generating PDF...");
+                            ReportGenerator.Export(report, output);
+                            Thread.Sleep(1000);
+                        }
+                        catch (Exception ex)
+                        {
+                            AnsiConsole.WriteException(ex, ExceptionFormats.ShortenEverything);
+                            Environment.Exit(1);
+                        }
+                    }
+                );
+
+            AnsiConsole.Write(new TextPath($"Report saved to: {Path.GetFullPath(output)}"));
+
+            return 0;
+        }
+
+        public sealed class Settings : CommandSettings
+        {
+            /// <summary>
+            ///     Path of JSON file.
+            /// </summary>
+            [Description("Path of JSON file.")]
+            [CommandOption("-i|--input")]
+            public string InputFile { get; init; }
+
+            /// <summary>
+            ///     Path of PDF file.
+            /// </summary>
+            [Description("Path of PDF file.")]
+            [CommandOption("-o|--output")]
+            public string OutputFile { get; init; }
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,75 +1,18 @@
-﻿using System;
-using System.IO;
-using Utility.CommandLine;
+﻿using Spectre.Console.Cli;
 
 namespace GitlabReports
 {
     public static class Program
     {
-        /// <summary>
-        ///     Displays help text and exits.
-        /// </summary>
-        [Argument('h', "help", "Displays help text and exits.")]
-        private static bool ShowHelpText { get; set; }
-
-        /// <summary>
-        ///     Path of configuration file.
-        /// </summary>
-        [Argument('i', "input", "Path of JSON file.")]
-        private static string InputFile { get; set; }
-
-        /// <summary>
-        ///     Path of configuration file.
-        /// </summary>
-        [Argument('o', "output", "Path of PDF file.")]
-        private static string OutputFile { get; set; }
-
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionTrapper;
-
-            Arguments.Populate();
-
-            if (ShowHelpText || args.Length == 0)
+            var app = new CommandApp<GenerateReportCommand>();
+            app.Configure(c =>
             {
-                ShowHelp();
-                return;
-            }
-
-            if (string.IsNullOrEmpty(InputFile))
-            {
-                throw new ArgumentException($"'{nameof(InputFile)}' cannot be null or empty.", nameof(InputFile));
-            }
-
-            if (string.IsNullOrEmpty(OutputFile))
-            {
-                OutputFile = Path.ChangeExtension(InputFile, ".pdf");
-            }
-
-            var report = ReportGenerator.Import(InputFile);
-            ReportGenerator.Export(report, OutputFile);
-        }
-
-        private static void UnhandledExceptionTrapper(object sender, UnhandledExceptionEventArgs e)
-        {
-            Console.WriteLine($"ERROR:\t{((Exception)e.ExceptionObject).Message}");
-            Environment.Exit(1);
-        }
-
-        /// <summary>
-        ///     Show help for arguments.
-        /// </summary>
-        private static void ShowHelp()
-        {
-            var helpAttributes = Arguments.GetArgumentInfo(typeof(Program));
-
-            Console.WriteLine("Short\t   Long   \tFunction");
-            Console.WriteLine("-----\t----------\t--------");
-
-            foreach (var item in helpAttributes)
-            {
-                Console.WriteLine($"-{item.ShortName}\t--{item.LongName,-10}\t{item.HelpText}");
-            }
+                c.AddExample(new[] {"-i", "report.json"});
+                c.SetApplicationName("sr");
+            });
+            return app.Run(args);
         }
     }
 }
